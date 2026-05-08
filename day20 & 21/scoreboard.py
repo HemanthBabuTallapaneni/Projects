31ac65bd773fbd127316b58a2e7c4238a0fe9725
Tallapaneni Hemanth Babu <144312921+HemanthBabuTallapaneni@users.noreply.github.com>
2026-05-08 16:21:19 +0530

    Add high score functionality to scoreboard
    Load high score from file and update scoreboard display.

--- day20 & 21/scoreboard.py
+++ day20 & 21/scoreboard.py
@@ -1,11 +1,12 @@
 from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Arial",24,"normal")
-
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score =int(data.read())
         self.penup()
         self.goto(0,265)
         self.color("white")
@@ -13,14 +14,22 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"SCORE: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"SCORE: {self.score}  High Score : {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode = "w") as data:
+                data.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
