09c2193ae04ac3a9f0b6d853158d110c32480f86
Tallapaneni Hemanth Babu <144312921+HemanthBabuTallapaneni@users.noreply.github.com>
2026-05-09 11:19:33 +0530

    Add US States Game functionality
    Implement US States Game using turtle graphics and pandas.

---
+++ day 25/main.py
@@ -0,0 +1,35 @@
+import turtle
+import pandas
+from pandas.core.dtypes import missing
+
+screen = turtle.Screen()
+screen.title("US States Game")
+image ="blank_states_img.gif"
+screen.addshape(image)
+screen.setup(width=725, height=492)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_states = []
+
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_missing.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
