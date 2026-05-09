3323641f9c81021972d9bcb9bef40a6d8f2d88dc
Tallapaneni Hemanth Babu <144312921+HemanthBabuTallapaneni@users.noreply.github.com>
2026-05-09 11:48:29 +0530

    Simplify missing states collection
    Refactor missing states logic to use list comprehension.

--- day 25/main.py
+++ day 25/main.py
@@ -16,15 +16,15 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
     if answer_state == "Exit":
-        missing_states = []
-
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
-
+        # missing_states = []
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
+        missing_states = [state for state in all_states if state  not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_missing.csv")
         break
+        
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
