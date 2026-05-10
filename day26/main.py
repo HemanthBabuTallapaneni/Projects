04084b95b4858ceb14354b34aebb1f9ad2934232
Tallapaneni Hemanth Babu <144312921+HemanthBabuTallapaneni@users.noreply.github.com>
2026-05-10 12:42:10 +0530

    Add generate_phonetic function with error handling
    Refactor code to add a function for generating phonetic output and handle exceptions for non-letter inputs.

--- day26/main.py
+++ day26/main.py
@@ -1,3 +1,4 @@
+#============= DAY 26 ================#
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
@@ -8,3 +9,24 @@ phonetic_dict = {row.letter: row.code for (index,  row) in data.iterrows()}
 word = input("Enter a word: ").upper()
 output_list = [phonetic_dict[letter] for letter in word]
 print(output_list)
+
+#============ DAY 30 =================#
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+print(data.to_dict())
+
+phonetic_dict = {row.letter: row.code for (index,  row) in data.iterrows()}
+print(phonetic_dict)
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, Only letters allowed.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
