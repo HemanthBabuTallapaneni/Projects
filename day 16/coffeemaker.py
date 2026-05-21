ffdeca1146dd5f5b291899edb6b2dd3ec5e0f075
Tallapaneni Hemanth Babu <144312921+HemanthBabuTallapaneni@users.noreply.github.com>
2026-05-21 13:18:51 +0530

    Remove docstrings from CoffeeMaker methods
    Removed docstrings from methods in CoffeeMaker class.

--- day 16/coffeemaker.py
+++ day 16/coffeemaker.py
@@ -1,5 +1,4 @@
 class CoffeeMaker:
-    """Models the machine that makes the coffee"""
     def __init__(self):
         self.resources = {
             "water": 300,
@@ -14,7 +13,6 @@ class CoffeeMaker:
         print(f"Coffee: {self.resources['coffee']}g")
 
     def is_resource_sufficient(self, drink):
-        """Returns True when order can be made, False if ingredients are insufficient."""
         can_make = True
         for item in drink.ingredients:
             if drink.ingredients[item] > self.resources[item]:
@@ -23,7 +21,6 @@ class CoffeeMaker:
         return can_make
 
     def make_coffee(self, order):
-        """Deducts the required ingredients from the resources."""
         for item in order.ingredients:
             self.resources[item] -= order.ingredients[item]
         print(f"Here is your {order.name} ☕️. Enjoy!")
